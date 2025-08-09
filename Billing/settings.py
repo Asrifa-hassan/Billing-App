1c1fb1aebb91582819ca39805e304730ae5a0b14
Asrifa-hassan <achiachu1122@gmail.com>
2025-08-09 11:39:25 +0530

    initial-commit

--- Billing/settings.py
+++ Billing/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'myapp.middleware.DisableCacheForAuthUsersMiddleware',
+    'Billing_App.middleware.DisableCacheForAuthUsersMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
